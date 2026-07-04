3098a13a14186668cab1dd84a51746958a35c3b9
Ashkan Rahmani <ashkan@tes.ca>
2026-07-04 03:34:54 +0330

    add social redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,9 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  redirects: {
+    "/ig": "https://instagram.com/happy_developer",
+    "/yt": "https://youtube.com/happy_developer",
+    "/tlg": "https://t.me/happydeveloper_tlg",
+  },
 });
